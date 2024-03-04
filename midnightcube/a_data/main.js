10fd49b9e834e00823b5b1fd1bd0da6dac076d06
Ian Collins <lancol@protonmail.com>
2024-03-03 20:55:12 -0800

    fixing midnightcube canvas size and experimenting with new outer cube

--- midnightcube/a_data/main.js
+++ midnightcube/a_data/main.js
@@ -43,7 +43,7 @@ var light = new THREE.PointLight(0xffffff);
 scene.add(light);
 
 const imgsrc = "../../images/live-midnight-reaction.jpg";
-const skysrc = "../../images/spacebackground.gif"
+const skysrc = "../../images/velvetcube.jpg"
   
 var loader = new THREE.TextureLoader();
 loader.load(imgsrc, function(tex) {
