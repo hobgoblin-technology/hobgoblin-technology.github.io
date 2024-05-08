5204cc8e44035d3cc245012d83851885d5356a9d
Ian Collins <lancol@protonmail.com>
2024-05-08 02:55:38 -0700

    changing midnight's containment chamber

--- midnightcube/a_data/main.js
+++ midnightcube/a_data/main.js
@@ -43,7 +43,7 @@ var light = new THREE.PointLight(0xffffff);
 scene.add(light);
 
 const imgsrc = "../../images/live-midnight-reaction.jpg";
-const skysrc = "../../images/velvet.jpg"
+const skysrc = "../../images/containment.jpg";
   
 var loader = new THREE.TextureLoader();
 loader.load(imgsrc, function(tex) {
